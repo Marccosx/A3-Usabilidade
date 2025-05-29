d55c1e11b3af93108b45eccf236debd6d50d118c
Marcos Guilherme Silva da Cruz <marcoscruz@neojiba.local>
2025-05-29 14:18:19 -0300

    criação dos models de controllers de endereço  e cidade

---
+++ src/API/models/cidadeModel.js
@@ -0,0 +1,45 @@
+const db = require('../databases/connection');
+
+const cidadeModel = {
+    create: (nome, id_estado, callback)=>{
+        db.run(`INSERT INTO cidade (nome, id_estado) VALUES (?, ?)`,
+            [nome, id_estado],
+            function(err){
+                if (err) return callback(err);
+                callback(null, {id: this.lastID});
+            }
+        );
+    },
+    list: (callback)=>{
+        db.all(`SELECT * FROM cidade`, [], (err, rows)=>{
+            if(err) return callback(err);
+            callback(null, rows);
+        });
+    },
+    buscarCidadePorId: (id,callback)=> {
+        db.get(`SELECT * FROM cidade where id = ?`, [id], (err, row)=>{
+            if (err) return callback(err);
+            callback (null, row);
+        });
+    },
+    update: (id,nome,id_estado,callback)=>{
+        db.run(
+            `UPDATE cidade SET nome=?, id_estado=?  WHERE id=?` [nome, id_estado, id],
+            function (err){
+                if(err) return callback(err);
+                callback(null, {changes: this.changes});
+            }
+        );
+    },
+    delete: (id, callback)=>{
+        db.run(
+            `DELETE FROM cidade WHERE id=?`,
+            [id],
+        function (err){
+            if(err) return callback(err);
+            callback(null, {changes: this.changes})
+        });
+    }
+};
+
+module.exports= cidadeModel;
